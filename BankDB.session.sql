414d5af7c4f047aa68604c2360456bf3a5b239f5
safiullahmahmoodzai1 <49817998+safiullahmahmoodzai1@users.noreply.github.com>
2022-04-15 22:48:51 -0500

    Add files via upload
    Bank management system

---
+++ BankDB.session.sql
@@ -0,0 +1,14 @@
+use Bank; 
+
+INSERT INTO `Bank` VALUES (2,'Ken','6756757','sd@test.com','23234 sdfge', 'Houston','US', 23423);
+INSERT INTO `Bank` VALUES (3,'Jame','376767','sd@test.com','45 sdfge', 'Houston','US', 23423);
+INSERT INTO `Bank` VALUES (4,'Kirbik','3455234','sd@test.com','24433 sdfge', 'Houston','US', 23423);
+INSERT INTO `Bank` VALUES (5,'Nest','3434234','sd@test.com','55 sdfge', 'Houston','US', 23423);
+INSERT INTO `Bank` VALUES (6,'West','365674','sd@test.com','23545 sdfge', 'Houston','US', 23423);
+INSERT INTO `Bank` VALUES (7,'Bee','3434234','sd@test.com','23 sdfge', 'Houston','US', 23423);
+INSERT INTO `Bank` VALUES (8,'See','3434234','sd@test.com','23 sdfge', 'Houston','US', 23423);
+INSERT INTO `Bank` VALUES (9,'KA','3434234','sd@test.com','23 sdfge', 'Houston','US', 23423);
+INSERT INTO `Bank` VALUES (10,'Kon','3434234','sd@test.com','23 sdfge', 'Houston','US', 23423);
+
+
+
